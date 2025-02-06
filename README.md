53f76b63b4b8a96deea0fb8f3996c73566104f76
Omer Chatzi Ali <omerhaciali1@gmail.com>
2025-02-06 21:56:16 +0200

    Dosya yapısını düzelttim

---
+++ README.md
@@ -0,0 +1,28 @@
+# TeeXpress 3D React
+
+This project is a 3D customization tool that allows users to design personalized t-shirts. Users can choose the color of their t-shirts, customize their designs with logos and full-surface printing. The interface offers a powerful experience with user-friendly animations and interactive features.
+
+## Key Features:
+
+- Color Selection: Users can instantly choose the color of their t-shirts. Colors can be easily changed with modern tools like SketchPicker.
+
+- Logo Upload: Users can upload any logo or image they want and place it on their t-shirts.
+
+- Personalization and Design: Users get instant feedback when customizing their designs and can quickly make any changes they want to the design.
+
+- File Upload: Users can directly select the images they want to upload with the file upload feature and edit them from the app.
+
+## Technologies and Libraries Used
+
+- React: React was used to create the user interface.
+- Valtio: For state management, Valtio was used to provide a centralized state repository.
+- Tailwind CSS: Tailwind CSS was preferred for fast and flexible styling applications.
+- Framer Motion: An interactive experience was created using Framer Motion for animation and transitions.
+- Three: Used to create 3D graphics and scenes.
+- React-three: Used to integrate 3D elements with React.
+- Maath: Used for mathematical calculations and vector operations in 3D projects.
+- React-Dom: Used to perform rendering operations on the DOM.
+
+## GIF
+
+![Project GIF](public/REC-20250206205217-ezgif.com-video-to-gif-converter.gif)
---
+++ src/pages/Home.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+import state from "../store";
+import { CustomButton } from "../components";
+
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                HAYDİ <br className="xl:block hidden" /> DURMA!
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-500 text-base">
+                Yepyeni 3D kişiselleştirme aracımızla benzersiz ve özel
+                tişörtünü tasarla. <strong>Hayal gücünü serbest bırak</strong>{" "}
+                ve kendi tarzını oluştur.
+              </p>
+              <CustomButton
+                type="filled"
+                title="Özelleştir"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
